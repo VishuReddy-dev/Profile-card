2c10bfeb72017b65e9589be2d458412dd63e1347
unknown <saduvishwanath95@gmail.com>
2025-02-26 08:21:36 +0530

    minimal changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
         description="Web and Mobile Developer and a Tech Enthusiast"
         github="https://github.com/VishuReddy-dev/"
         linkedin="https://www.linkedin.com/in/vishwanath-reddy-780947256/"
-        Image="src\assets\IMG_20230407_215040_753.jpg"
+        Image="src/assets/IMG_20230407_215040_753.jpg"
       />
     </div>
   );
